9a9b5a24be7260f6cde076317c49e0904fff80fd
Ponrawoot <158698118+Min-pon@users.noreply.github.com>
2024-03-07 20:49:41 +0700

    add component template

---
+++ src/components/AdvertisingCard.jsx
@@ -0,0 +1,3 @@
+export default function AdvertisingCard() {
+  return <div></div>;
+}
---
+++ src/components/TestComponent.jsx
@@ -0,0 +1,3 @@
+export default function TestComponent() {
+  return <img src="images/placeholder.png" alt="placeholder" width={100} />;
+}
