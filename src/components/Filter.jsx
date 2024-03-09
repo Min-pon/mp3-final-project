3cd36625bb382011cf59ecf5e2dd043bed80d192
parameth <palm24572@gmail.com>
2024-03-09 20:33:05 +0700

    responsive cardProduct

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,3 +1,37 @@
-export default function Filter() {
-  return <div></div>;
+import React from "react";
+
+function Filter() {
+  return (
+    <div className="join join-vertical w-full">
+      <div className="collapse collapse-arrow join-item border border-base-300">
+        <input type="radio" name="my-accordion-4" defaultChecked />
+        <div className="collapse-title text-xl font-medium">
+          Click to open this one and close others
+        </div>
+        <div className="collapse-content">
+          <p>hello</p>
+        </div>
+      </div>
+      <div className="collapse collapse-arrow join-item border border-base-300">
+        <input type="radio" name="my-accordion-4" />
+        <div className="collapse-title text-xl font-medium">
+          Click to open this one and close others
+        </div>
+        <div className="collapse-content">
+          <p>hello</p>
+        </div>
+      </div>
+      <div className="collapse collapse-arrow join-item border border-base-300">
+        <input type="radio" name="my-accordion-4" />
+        <div className="collapse-title text-xl font-medium">
+          Click to open this one and close others
+        </div>
+        <div className="collapse-content">
+          <p>hello</p>
+        </div>
+      </div>
+    </div>
+  );
 }
+
+export default Filter;
--- src/pages/ItemProductList.jsx
+++ src/pages/ItemProductList.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { useParams  } from 'react-router-dom'
 import ProductCard from "../components/ProductCard"
+import Filter from '../components/Filter';
 
 function ItemProductList() {
   const { type } = useParams();
   return (
-    <>
+    <div className=' flex justify-center p-7'>
     {/* <div>ItemProductList {type}</div> */}
     <ProductCard />
-    </>
+    <Filter />
+    </div>
     
   )
 }
