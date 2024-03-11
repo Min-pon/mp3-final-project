573f81ab2484a7f54c72c74ddf3617cd96d108d7
Weraphat Nimchaiyanan <35192556+petepeach@users.noreply.github.com>
2024-03-11 22:02:40 +0700

    create-cart-items-structures

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,3 +1,12 @@
-export default function CartItem() {
-  return <div></div>;
+export default function CartItem({}) {
+  return (
+    <div class="flex flex-row gap-2">
+      <div class="flex w-1/5 aspect-square bg-fuchsia-200">img</div>
+      <div class="flex flex-col grow bg-white p-4">
+        detail
+        <div>name</div>
+        <div>selector</div>
+      </div>
+    </div>
+  );
 }
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,3 +1,16 @@
+import CartItem from "../components/CartItem";
+
 export default function Cart() {
-  return <div>Cart Page</div>;
+  return (
+    <div>
+      Cart Page
+      <div class="flex flex-row gap-2 w-full p-8 bg-red-500 ...">
+        <div class="grow bg-blue-200 p-4 flex flex-col gap-2">
+          items
+          <CartItem> item 1</CartItem>
+        </div>
+        <div class="w-1/4 bg-slate-100 p-4">summary</div>
+      </div>
+    </div>
+  );
 }
