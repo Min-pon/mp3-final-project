364029d64a915beec3b557b44ed7f0d3a2b0f9e0
Weraphat Nimchaiyanan <35192556+petepeach@users.noreply.github.com>
2024-03-14 20:47:10 +0700

    structurePage

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,3 +1,12 @@
 export default function ProductDetail() {
-  return <div>Product Detail Page</div>;
+  return (
+    <div>
+      Product Detail Page
+      <div className="container m-auto grid grid-cols-2 grid-rows-2 ">
+        <div className="row-start-1 row-end-3 bg-red-500 m-1">one</div>
+        <div className=" bg-yellow-500 m-1">two</div>
+        <div className=" bg-green-500 m-1">two</div>
+      </div>
+    </div>
+  );
 }
