e972aea08f7eeb07cea0219fa270ae3d3cf12668
Ponrawoot <158698118+Min-pon@users.noreply.github.com>
2024-03-18 15:46:39 +0700

    update text size and color in component

--- src/components/AdvertisingCard.jsx
+++ src/components/AdvertisingCard.jsx
@@ -4,11 +4,11 @@ export default function AdvertisingCard({ imgUrl, header, detail }) {
 
   return (
     <div
-      className={`text-white bg-[url("${imgUrl}")] h-[500px] w-[575px] bg-cover flex flex-col justify-end items-center space-y-[16px] pb-[16px]`}
+      className={`text-white bg-[url("https://cdn.discordapp.com/attachments/1120391488484933705/1216804605891973182/image.png?ex=6601b899&is=65ef4399&hm=e031710e6cafa5d1754d337276214480bbab4583014d8a8ddeb9251e9904a808&")] h-[500px] w-[575px] bg-cover flex flex-col justify-end items-center space-y-[16px] pb-[16px]`}
     >
-      <p className="text-[24px] font-bold">{header}</p>
-      <p className="text-[16px] font-normal px-[16px] text-center">{detail}</p>
-      <button className="bg-[#222222] px-[10px] py-[17px] w-[105px]">
+      <p className="text-h6 font-bold">{header}</p>
+      <p className="text-body font-normal px-[16px] text-center">{detail}</p>
+      <button className="bg-secondary px-[10px] py-[17px] w-[105px] text-body">
         View more
       </button>
     </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,39 +1,60 @@
+import { ArrowRightIcon } from "../assets/iconList";
+
 export default function Footer() {
   return (
-    <div className="bg-[#222222] flex flex-col px-[160px] py-[24px] text-white space-y-[16px]">
-      <div className="flex justify-between">
+    <div
+      className="bg-secondary
+     flex flex-col px-[160px] py-[24px] text-white space-y-[16px]"
+    >
+      <div className="flex justify-between gap-x-[45px]">
         <div>
           <div className="mb-[24px]">
-            <p className="text-[32px] font-bold">Featured product</p>
+            <p className="text-h5 font-bold">Featured product</p>
           </div>
           <div className="flex flex-col space-y-[16px]">
-            <p className="text-[18px] font-semibold">Men</p>
-            <p className="text-[18px] font-semibold">Ladies</p>
-            <p className="text-[18px] font-semibold">Shoes</p>
-            <p className="text-[18px] font-semibold">Accessories</p>
+            <p className="text-sub font-semibold">Men</p>
+            <p className="text-sub font-semibold">Ladies</p>
+            <p className="text-sub font-semibold">Shoes</p>
+            <p className="text-sub font-semibold">Accessories</p>
           </div>
         </div>
         <div>
           <div className="mb-[24px]">
-            <p className="text-[32px] font-bold">Customer services</p>
+            <p className="text-h5 font-bold">Register with us</p>
+          </div>
+          <div className="flex flex-col space-y-[24px]">
+            <p className="text-body font-normal">
+              Sign up now and get 20% off your first purchase!
+            </p>
+            <button className="flex bg-white px-[10px] py-[7px] gap-x-[8px] items-center w-[166px] h-[54px] ">
+              <p className="text-[16px] font-normal text-secondary">
+                Sign up now
+              </p>
+              <ArrowRightIcon />
+            </button>
+          </div>
+        </div>
+        <div className="w-[500px]">
+          <div className="mb-[24px]">
+            <p className="text-h5 font-bold">Customer services</p>
           </div>
           <div className="flex flex-col space-y-[16px]">
-            <p className="text-[16px] font-normal">
+            <p className="text-body font-normal">
               MBK Tower 20th Floor, 444, Phaya Thai Rd, Wang Mai, Pathum Wan,
               Bangkok 10330
             </p>
-            <p className="text-[16px] font-normal">
+            <p className="text-body font-normal">
               Email: jane.doe@realmail.com
             </p>
           </div>
         </div>
       </div>
       <div className="flex justify-between">
-        <p className="text-[12px] font-normal text-[#9F9F9F]">
+        <p className="text-caption font-normal text-[#9F9F9F]">
           Copyright © 2024 All rights reserved for all contents.
         </p>
         <div className="flex items-center">
-          <p className="text-[12px] text-[#9F9F9F]">Powered By</p>
+          <p className="text-caption text-[#9F9F9F]">Powered By</p>
           <img
             src="https://cdn.discordapp.com/attachments/1120391488484933705/1216800270709686374/image.png?ex=6601b48f&is=65ef3f8f&hm=50d4316bacc41b6208149879b62010ab8692068b69ba361a7dde7ad4dedb9fb2&"
             alt="logo2"
