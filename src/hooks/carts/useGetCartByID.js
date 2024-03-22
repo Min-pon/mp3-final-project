5461815247534792c7e2a0fe2f94c29b692d35cf
Ponrawoot <158698118+Min-pon@users.noreply.github.com>
2024-03-22 17:00:33 +0700

    use getCartByID api

--- src/hooks/carts/useGetCartByID.js
+++ src/hooks/carts/useGetCartByID.js
@@ -1,1 +1,30 @@
-export default function useGetCartByID() {}
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const BASE_URL = import.meta.env.VITE_BASE_API;
+
+export default function useGetCartByID(id) {
+  const [cart, setCart] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchCartByID = async () => {
+      try {
+        const response = await axios.get(`${BASE_URL}/carts/${id}`);
+        setCart(response.data);
+        console.log(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (!cart.length) {
+      fetchCartByID();
+    }
+  }, []);
+
+  return { cart, loading };
+}
