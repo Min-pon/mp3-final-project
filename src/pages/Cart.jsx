7b8c802d058e3e791d6bc0a88f5fc0a1a8927e3b
PhattarawadeeR <153511627+PhattarawadeeR@users.noreply.github.com>
2024-03-26 22:13:11 +0700

    Add campaign bar

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -4,6 +4,7 @@ import SummaryCard from "../components/SummaryCard";
 import ProductCardAlsoLike from "../components/ProductCardAlsoLike";
 import { useStore } from "../hooks/useStore";
 import useGetCartByID from "../hooks/carts/useGetCartByID";
+import CampaignBar from "../components/CampaignBar";
 
 export default function Cart() {
   const { cartId } = useStore((state) => ({
@@ -19,13 +20,15 @@ export default function Cart() {
   }
   return (
     <div className="bg-secondary-50 overflow-hidden">
+      <div>
+        <CampaignBar />
+      </div>
       <div className="mt-[41px] ml-[160px]">
         <div className="pl-[24px] text-[32px] font-bold">My cart</div>
         <div className="flex flex-row items-start mt-[41px] gap-x-10">
           <div className="p-[24px] text-[24px] font-bold bg-white">
             {cart.id ? (
               <div>
-                {" "}
                 <p>Items</p>
                 <CartItem />
               </div>
