ec94794d9259def925dce94f761f53cef23d13db
parameth <palm24572@gmail.com>
2024-03-28 09:21:14 +0700

    Update ProductDetail.jsx

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -143,19 +143,13 @@ export default function ProductDetail() {
   if (!product || data === null) return <div>No product data found</div>;
 
   return (
-    <div className="bg-white mt-[93px]">
-      <div className="flex flex-row gap-10">
-        {/* <div className="flex flex-col w-1/2 gap-2"> */}
+    <div className="bg-white mt-[93px] px-40">
+      <div className="flex flex-row justify-between">
         <div className="">
-          {/* Main image */}
           <ShowImageProduct product={product} />
-          {/* Thumbnails */}
         </div>
 
-        {/* Product info */}
-        <div className="mt-2 px-4 sm:px-0 sm:mt-16 lg:mt-0 w-1/2 flex flex-col gap-20">
-          {/* Product Detail */}
-
+        <div className="mt-2 px-4 max-w-[780px] sm:px-0 sm:mt-16 lg:mt-0 w-1/2 flex flex-col gap-20">
           <ProductInformation
             id={product.id}
             productName={product.name}
@@ -165,14 +159,13 @@ export default function ProductDetail() {
             ratings={product.ratings}
           />
           <div className="flex flex-col gap-2">
-            <FormControl>
-              {/* Color of item */}
-              <div className="mb-4">
-                <p className="text-lg font-semibold mb-2">Color</p>
-                <div className="flex space-x-2">
-                  {productDetail.map((color, idx) => (
+            {/* Color of item */}
+            <div className="mb-4">
+              <p className="text-lg font-semibold mb-2">Color</p>
+              <div className="flex space-x-7">
+                {productDetail.map((color, idx) => (
+                  <div key={idx} className=" flex w-[100px] flex-col justify-center items-center text-center gap-2">
                     <button
-                      key={idx}
                       className={` w-[54px] aspect-square rounded-full transition-all duration-200 ease-out  ${
                         selectedColor === color
                           ? " ring-2 ring-primary"
@@ -188,74 +181,75 @@ export default function ProductDetail() {
                       }}
                       aria-label={`Select ${color.name}`}
                     />
+                    <p>{color.color}</p>
+                  </div>
+                ))}
+              </div>
+            </div>
+
+            {/* size of item */}
+            {selectedColor.variants[0].size && (
+              <div className="mb-4">
+                <div className="text-lg font-semibold mb-2">Size</div>
+                <div className="flex flex-row space-x-2">
+                  {selectedColor.variants.map((size, idx) => (
+                    <button
+                      key={idx}
+                      className={`grow px-4 py-2 border disabled:border-gray-100 disabled:text-gray-300 disabled:bg-gray-100 border-gray-300 focus:outline-none ${
+                        selectedSize === size ? "ring-2 ring-[#DEF81C]" : ""
+                      }`}
+                      onClick={() => {
+                        if (size !== selectedSize) {
+                          setSelectedSize(size);
+                          setSelectQuantity(1);
+                        }
+                      }}
+                      disabled={size.remains < 1}
+                    >
+                      {size.size}
+                    </button>
                   ))}
                 </div>
               </div>
+            )}
 
-              {/* size of item */}
-              {selectedColor.variants[0].size && (
-                <div className="mb-4">
-                  <div className="text-lg font-semibold mb-2">Size</div>
-                  <div className="flex flex-row space-x-2">
-                    {selectedColor.variants.map((size, idx) => (
-                      <button
-                        key={idx}
-                        className={`grow px-4 py-2 border disabled:border-gray-100 disabled:text-gray-300 disabled:bg-gray-100 border-gray-300 focus:outline-none ${
-                          selectedSize === size ? "ring-2 ring-[#DEF81C]" : ""
-                        }`}
-                        onClick={() => {
-                          if (size !== selectedSize) {
-                            setSelectedSize(size);
-                            setSelectQuantity(1);
-                          }
-                        }}
-                        disabled={size.remains < 1}
-                      >
-                        {size.size}
-                      </button>
-                    ))}
-                  </div>
-                </div>
-              )}
-
-              {/* Quantity of item */}
-              <div className="mb-4">
-                <label
-                  htmlFor="quantity"
-                  className="text-lg font-semibold mb-2 block"
+            {/* Quantity of item */}
+            <div className="mb-4">
+              <label
+                htmlFor="quantity"
+                className="text-lg font-semibold mb-2 block"
+              >
+                Qty
+              </label>
+              <div className=" flex">
+                <select
+                  id="quantity"
+                  className="basis-1/4 block w-full p-2 text-base border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
+                  value={selectquantity}
+                  onChange={(e) => setSelectQuantity(e.target.value)}
                 >
-                  Qty
-                </label>
-                <div className=" flex">
-                  <select
-                    id="quantity"
-                    className="basis-1/4 block w-full p-2 text-base border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
-                    value={selectquantity}
-                    onChange={(e) => setSelectQuantity(e.target.value)}
-                  >
-                    {quantities.map((q) => (
-                      <option key={q} value={q}>
-                        {q}
-                      </option>
-                    ))}
-                  </select>
-                </div>
+                  {quantities.map((q) => (
+                    <option key={q} value={q}>
+                      {q}
+                    </option>
+                  ))}
+                </select>
               </div>
+            </div>
 
-              {/* Add to cart button */}
-              <div className="mt-10 flex flex-col gap-4">
-                <button
-                  type="submit"
-                  className="flex justify-center items-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-black hover:bg-gray-800"
-                  onClick={() => {
-                    openModal();
-                    handleSubmit();
-                  }}
-                >
-                  Add to cart
-                </button>
-              </div>
-            </FormControl>
+            {/* Add to cart button */}
+            <div className="mt-10 flex flex-col gap-4">
+              <button
+                type="submit"
+                className="flex justify-center items-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-black hover:bg-gray-800"
+                onClick={() => {
+                  openModal();
+                  handleSubmit();
+                }}
+              >
+                Add to cart
+              </button>
+            </div>
 
             <ShoppingCartModal
               isOpen={isModalOpen}
