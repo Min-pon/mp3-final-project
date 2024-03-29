dc563a32a832f1178a85dbbec7282d3935b75faa
Ponrawoot <158698118+Min-pon@users.noreply.github.com>
2024-03-29 17:30:55 +0700

    add empty card image

--- src/components/EmptyCard.jsx
+++ src/components/EmptyCard.jsx
@@ -5,10 +5,7 @@ export default function EmptyCard() {
   return (
     <div className="flex flex-col items-center w-[896px] h-[730px] mt-[24px]">
       <div className="w-[403px] h-[403px]">
-        <img
-          src="https://cdn.discordapp.com/attachments/1208360755007852586/1217848591305932840/image.png?ex=660584e2&is=65f30fe2&hm=133903bce19e4a09f025def30965b10c05a275eaca3960dc785e125528d5fedf&"
-          alt="Empty cart"
-        />
+        <img src="../images/empty_card.png" alt="Empty cart" />
       </div>
       <div className="text-[40px] font-bold mt-[24px]">Your cart is empty</div>
       <p className="text-center text-[18px] font-semibold mt-[8px]">
